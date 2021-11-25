b472c3330e1a6a92508c9060446bf1f9b60a6103
Larissa71 <larissakuffel@gmail.com>
2021-11-25 09:02:16 -0300

    25/11

--- paginauser/index.js
+++ paginauser/index.js
@@ -5,16 +5,25 @@ let username2 = username1.replace('?', '');
 
 let imagem = document.createElement('img');
 let nomeuser = document.createElement('p');
+let usuario = document.createElement('p');
+
+let listarep;
 
 function getUserGithub(userName) {
-    fetch('https://fake-github.herokuapp.com/api/' + userName)
+    fetch('https://fake-github.herokuapp.com/api/search/' + userName)
         .then(function (resultado) {
             resultado.json().then(function (data) {
+
+                console.log(data);
+
                 imagem.src = data.avatar_url;
+                imagem.className = 'avatar';
 
                 nomeuser.innerText = data.name;
-                console.log(data.name);
+                nomeuser.className = 'nomeuser';
                 
+                usuario.innerText = data.login;
+                usuario.className = 'username';
             });
         }).catch(function (erro) {
             console.log('erro:', erro);
@@ -22,10 +31,11 @@ function getUserGithub(userName) {
 }
 
 function getUserReposGithub(userName) {
-    fetch('https://fake-github.herokuapp.com/api/' + userName + '/repos')
+    fetch('https://fake-github.herokuapp.com/api/search/' + userName + '/repos')
         .then(function (resultado) {
             resultado.json().then(function (data) {
                 console.log('Repositories Data:', data);
+                listarep = data;
             });
         }).catch(function (erro) {
             console.log('erro:', erro);
@@ -35,22 +45,25 @@ function getUserReposGithub(userName) {
 getUserGithub(username2);
 getUserReposGithub(username2);
 
+let esquerda;
+let direita;
+
 function criarpagina() {
 
-    let esquerda = document.createElement('div');
+    esquerda = document.createElement('div');
     esquerda.className = 'esquerda';
 
-    let direita = document.createElement('div');
+    direita = document.createElement('div');
     direita.className = 'direita';
 
     tela.appendChild(esquerda);
     tela.appendChild(direita);
 
-    partedireita(direita);
-    parteesquerda(esquerda);
+    partedireita();
+    parteesquerda();
 }
 
-function partedireita(a) {
+function partedireita() {
 
     let divdireita1 = document.createElement('div');
     divdireita1.className = 'divdireita1'
@@ -60,18 +73,58 @@ function partedireita(a) {
     find.className = 'find';
 
     let type = document.createElement('button');
-    type.placeholder = 'Type';
+    type.innerText = 'Type';
     type.className = 'type';
+
+    let language = document.createElement('button');
+    language.innerText = 'Language';
+    language.className = 'language';
+
+    let sort = document.createElement('button');
+    sort.innerText = 'Sort';
+    sort.className = 'sort';
+
+    let newr = document.createElement('button');
+    newr.innerText = 'New';
+    newr.className = 'newr';
 
     divdireita1.appendChild(find);
     divdireita1.appendChild(type);
-    a.appendChild(divdireita1);
+    divdireita1.appendChild(language);
+    divdireita1.appendChild(sort);
+    divdireita1.appendChild(newr);
+    direita.appendChild(divdireita1);
+
+    repositorios();
 }
 
-function parteesquerda(a) {
+function parteesquerda() {
 
-    a.appendChild(imagem);
-    a.appendChild(nomeuser);
+    let nomeeuser = document.createElement('div');
+    nomeeuser.className = 'nomeeuser';
+
+    let edit = document.createElement('button');
+    edit.innerText = 'Edit profile';
+    edit.className = 'edit';
+
+    esquerda.appendChild(imagem);
+
+    nomeeuser.appendChild(nomeuser);
+    nomeeuser.appendChild(usuario);
+    nomeeuser.appendChild(edit);
+
+    esquerda.appendChild(nomeeuser);
+}
+
+function repositorios() {
+
+    listarep.forEach(a => {
+        
+        let linha = document.createElement('div');
+        linha.className = 'linharep';
+
+        direita.appendChild(linha);
+    });
 }
 
 criarpagina();
